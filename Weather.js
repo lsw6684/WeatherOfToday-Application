5c76512c609a26a9649adca668e844f8d7fd5e80
lsw6684 <lsw6684@hanmail.net>
2020-10-03 21:10:19 +0900

    using api...

---
+++ Weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+import PropTypes from "prop-types";
+
+export default function Weather({temp}){
+    return (
+      <View style = {styles.conainer}>
+    <Text>
+      {temp}
+    </Text>
+    </View>
+  );
+}
+
+Weather.propTypes = {
+    temp:PropTypes.number.isRequired
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1 ,
+    justifyContent: "center",
+    alighItems: "center"
+
+  }
+});
